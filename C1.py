3426495efec4e17f45ab1a3218670873c728ab49
arosworld <83116916+arosworld@users.noreply.github.com>
2021-04-24 13:07:00 +0200

    Add files via upload

---
+++ C1.py
@@ -0,0 +1,33 @@
+x = 5
+if x < 10:
+    print("smaller")
+if x > 20:
+    print("bigger")
+
+print("Finis")
+
+n = 5
+while n > 0:
+    print (n)
+    n= n-1
+print("Blastoff")
+
+# x = Hours, y = Rate, z = Pay
+x =35
+y = 2.75
+z = x * y
+print("Pay", z)
+
+c = 1 + 2 * 3 - 8 / 4
+print(c)
+
+x = int(98.6)
+print(x)
+
+print("123" + "abc")
+
+# Convert elevator floors
+inp = input("Europe floor?")
+usf = int(inp) + 1
+print("Us floor", usf)
+
